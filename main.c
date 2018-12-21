7ed40efdc98d5e8a6e0542591a887e9e3aff6517
Vivek Mehra <vivek.1376@gmail.com>
2018-12-20 19:19:43 -0500

    temp

--- main.c
+++ main.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <pthread.h>
+
 
 #define MAX_ELEMENTS 10000000
+
+struct thread_data {
+    long int *arr1, *arr2, *merged;
+    long int count1, count2;
+    unsigned long int i;
+};
+
 
 void mergeArrays(long int *arr1, long int count1, long int *arr2, long int count2, long int *merged)
 {
@@ -25,15 +34,37 @@ void mergeArrays(long int *arr1, long int count1, long int *arr2, long int count
 }
 
 
+void *thread_function(void *arg)
+{
+    struct thread_data *data = (struct thread_data *)arg;
+
+    mergeArrays(data->arr1, data->count1, data->arr2, data->count2, data->merged);
+    
+    return NULL;
+}
+
+
 void mergeSort(long int *arr, unsigned long int count)
 {
-    long int *merged = NULL, *arr1, *arr2;
+    long int *merged1 = NULL, *merged2 = NULL, *arr1, *arr2;
     unsigned long int count1, count2, s, i;
 
+    pthread_t tid;
+
+    struct thread_data tdata;
+    tdata.merged = NULL;
+    
+    int res;
+    
     /* main iterative loop */
     for (s = 1; s < count; s = s*2) {
+	printf("h1\n");
+	fflush(stdout);
 	fflush(stdout);
 	for(i = 0; i < count; i += (2*s)) {
+	    printf("h2\n");
+	    fflush(stdout);
+
 	    arr1 = arr + i;
 	    arr2 = arr + (i + s);
 	    count1 = count2 = s;
@@ -46,15 +77,45 @@ void mergeSort(long int *arr, unsigned long int count)
 		count2 = count - (i + s); 	/* override */
 	    }
 	    
-	    merged = (long int *)malloc(sizeof(long int) * (count1 + count2));
-	
-	    mergeArrays(arr1, count1, arr2, count2, merged);
+	    if (i%2 == 0) {
+		/* join */
+		printf("h3\n");
+		fflush(stdout);
+		
+		
+		if (tdata.merged != NULL) {
+		    res = pthread_join(tid, NULL);
+		    printf("h1 res=%d\n", res); //d
+		    fflush(stdout);		
+		    memcpy(arr + tdata.i, tdata.merged, sizeof(long int) * (tdata.count1 + tdata.count2));
+		    free(tdata.merged);
+		    tdata.merged = merged1 = NULL;
+		}
+		
+		merged1 = (long int *)malloc(sizeof(long int) * (count1 + count2));
+		
+		tdata.arr1 = arr1;
+		tdata.arr2 = arr2;
+		tdata.count1 = count1;
+		tdata.count2 = count2;
+		tdata.merged = merged1;
 
-	    /* copy */
-	    memcpy(arr + i, merged, sizeof(long int) * (count1 + count2));
+		res = pthread_create(&tid, NULL, thread_function, (void *)&tdata);
+		/* check res */
+	    } else {
+		merged2 = (long int *)malloc(sizeof(long int) * (count1 + count2));
+		
+		mergeArrays(arr1, count1, arr2, count2, merged2);
+		/* copy */
+		memcpy(arr + i, merged2, sizeof(long int) * (count1 + count2));
 
-	    free(merged);
-	    merged = NULL;
+		free(merged2);
+		merged2 = NULL;
+		//res = pthread_join(tid, NULL);
+
+		/* check res */
+		
+	    }
 	}
     }
 }
@@ -64,6 +125,7 @@ int main()
 {
     long int n;
     unsigned long int i, j;
+
     
     long int *arr = (long int *)malloc(MAX_ELEMENTS * sizeof(long int));
 
@@ -78,12 +140,14 @@ int main()
 	fprintf(stderr, "Too many numbers.\n");
 	return -1;
     }
-    
+
+    /* print input */
     /* for(j=0; j<i; j++) */
     /* 	printf("num=%ld\n", arr[j]); */
 
     mergeSort(arr, i);
 
+    /* print sorted */
     for(j=0; j<i; j++)
 	printf("%ld\n", arr[j]);
     
